89c31125422ab708e4079377d489eae55875495c
Paul Robinson <darkraver@gmail.com>
2012-11-05 16:19:19 +0000

    Moving scanSerial into its own module
    Moving scanSerial so it can be used by other things other than pyobd

---
+++ obd_recorder.py
@@ -0,0 +1,23 @@
+import obd_io
+import serial
+import platform
+
+from obd_utils import scanSerial
+
+class OBD_Recorder():
+    def connect(self):
+        portnames = scanSerial()
+        print portnames
+        for port in portnames:
+            self.port = obd_io.OBDPort(port, None, 2, 2)
+            if(self.port.State == 0):
+                self.port.close()
+                self.port = None
+            else:
+                break
+
+        if(self.port):
+            print "Connected to "+self.port.port.name
+            
+o = OBD_Recorder()
+o.connect()
