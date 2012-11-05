2df3b90997d78f290ea63f9efbd4f84d608ffb58
Paul Robinson <darkraver@gmail.com>
2012-11-05 17:37:04 +0000

    Adding logger and is_connected function to recorder

--- obd_recorder.py
+++ obd_recorder.py
@@ -1,10 +1,20 @@
 import obd_io
 import serial
 import platform
+import logging
 
 from obd_utils import scanSerial
 
 class OBD_Recorder():
+    def __init__(self, filename):
+        self.port = None
+        self.logger = logging.getLogger(__name__)
+        log_handler = logging.FileHandler(filename)
+        log_formatter = logging.Formatter('%(asctime)s %(message)s')
+        log_handler.setFormatter(log_formatter)
+        self.logger.addHandler(log_handler)
+        self.logger.setLevel(logging.INFO)
+        
     def connect(self):
         portnames = scanSerial()
         print portnames
@@ -19,5 +29,18 @@ class OBD_Recorder():
         if(self.port):
             print "Connected to "+self.port.port.name
             
-o = OBD_Recorder()
+    def is_connected(self):
+        return self.port
+        
+#    def record_data(self):
+#        for n in range(1,10):
+#            self.logger.info('woo %s %s %s yay', n, n, n)
+            
+            
+        
+o = OBD_Recorder('bikestuff.log')
 o.connect()
+if not o.is_connected():
+    print "Not connected"
+    
+#o.record_data()
